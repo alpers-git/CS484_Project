acb6f2bdf209f44bfbb041553bdfbd7e7adabfea
Serhat Aras <serhat.aras@ug.bilkent.edu.tr>
2018-12-29 15:01:10 +0300

    4.1 Finished

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #Part 3.1
 #   Data Reading
 import numpy as np
+import PIL
 from PIL import Image
 import cv2
 import torch
@@ -12,16 +13,73 @@ sys.path.insert(0, 'data/')
 
 import resnet
 
-rootDir = 'data/train/'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    print('Found directory: %s' % dirName)
-    for fname in fileList:
-        print('\t%s' % fname)
+#for display and print
+def printImg(res_im):
+    print(res_im.shape)
+    rgb_img = cv2.cvtColor(res_im, cv2.COLOR_BGR2RGB)                                   #do this or displayed img will have color channels in BGR order
+    cv2.imshow('image',rgb_img)
+    cv2.waitKey(0)                                                                      #press a random button to close window DO NOT USE RED X !!!
+    cv2.destroyAllWindows()
 
-#image = cv2.imread('data/train/n01615121/n01615121_866.jpeg',1)
-#cv2.imshow('image',image)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+
+rootDir = 'data/train/'
+model = resnet.resnet50()
+
+
+t_ClassNames = []
+t_FeatureVectors = []
+
+for dirName, subdirList, fileList in os.walk(rootDir):
+    #print('Found directory: %s' % dirName)
+    className = dirName[len(rootDir):]
+    #print('\t%s' % className) 
+    for fname in fileList:
+        #print('\t%s' % fname) 
+        image = Image.open(dirName+ '/' +fname).convert('RGB')    
+        #print(image)
+        image_arr = np.asarray(image)   #convert to a numpy array
+        rgb_img = cv2.cvtColor(image_arr, cv2.COLOR_BGR2RGB)  #do this or displayed img will have color channels in BGR order   
+
+        #cv2.imshow('image',rgb_img)
+        #cv2.waitKey(0)                                                                      #press a random button to close window DO NOT USE RED X !!!
+        #cv2.destroyAllWindows()
+                                                        
+        org_size = image.size
+        max_dim = max(org_size[0], org_size[1])
+
+        padded_size = (max_dim, max_dim)
+        padded_im = Image.new("RGB", padded_size)
+        padded_im.paste(image, ( (int)((padded_size[0] - org_size[0])/2), 
+                                (int)((padded_size[1] - org_size[1])/2) ))                  #pastes image in middle of a black image
+
+        padded_im = np.asarray(padded_im)
+        res_im = cv2.resize(padded_im, dsize=(224, 224), interpolation=cv2.INTER_LANCZOS4)  #resizes to 224x224
+        res_im = np.asarray(res_im)
+        res_im = res_im.astype(np.float32)
+        
+        #Part 3.2
+        #   Feature extraction
+        #model = resnet.resnet50()
+        #we append an augmented dimension to indicate batch_size, which is one
+        res_im = np.reshape(res_im, [1, 224, 224, 3])
+        # model takes as input images of size [batch_size, 3, im_height, im_width]
+        res_im = np.transpose(res_im, [0, 3, 1, 2])
+        # convert the Numpy image to torch.FloatTensor
+        res_im = torch.from_numpy(res_im)
+        # extract features
+        feature_vector = model(res_im)
+        # convert the features of type torch.FloatTensor to a Numpy array
+        # so that you can either work with them within the sklearn environment
+        # or save them as .mat files
+        feature_vector = feature_vector.data.cpu().numpy()
+        
+
+        #write feature vector in to a structure
+        t_ClassNames.append(className)
+        t_FeatureVectors.append(feature_vector)
+
+        print(feature_vector)
+
 
 """image = Image.open('data/train/n01615121/n01615121_1705.jpeg').convert('RGB')       #load an image
 image_arr = np.asarray(image)                                                       #convert to a numpy array
@@ -56,11 +114,5 @@ feature_vector = model(res_im)
 feature_vector = feature_vector.data.cpu().numpy()
 #print(type(feature_vector))"""
 
-#for display and print
-"""print(res_im.shape)
-rgb_img = cv2.cvtColor(res_im, cv2.COLOR_BGR2RGB)                                   #do this or displayed img will have color channels in BGR order
-cv2.imshow('image',rgb_img)
-cv2.waitKey(0)                                                                      #press a random button to close window DO NOT USE RED X !!!
-cv2.destroyAllWindows()"""
 
 
